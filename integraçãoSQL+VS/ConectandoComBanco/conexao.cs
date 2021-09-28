942e42c38d7e41353f52c984c56aa3c75e35888e
Guilherme  Cezar <54600850+G648@users.noreply.github.com>
2021-09-28 16:55:23 -0300

    Add files via upload

---
+++ integraçãoSQL+VS/ConectandoComBanco/conexao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConectandoComBanco
+{
+    class Conexao
+    {
+        //Aqui vai a string para acessar o banco de dados
+        private static string ConnStr = @"Data Source=DESKTOP-AD6KDJ2\SERVIDORTARDE;Initial Catalog=BancoDeDados;Integrated Security=True";
+
+        //Método para conectar com a base de dados
+        public static SqlConnection GetConect()
+        {
+            var conn = new SqlConnection(ConnStr);
+
+            return conn;
+        }
+    }
+}
